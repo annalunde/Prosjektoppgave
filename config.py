38122117edcaa65999164bf9affaa8da1839411b
marialbr <marialbr@stud.ntnu.no>
2021-10-12 08:48:37 +0200

    Added mathematical model draft

---
+++ config.py
@@ -0,0 +1,27 @@
+# Model 1
+# Sets
+num_pickup_nodes = 1
+num_dropoff_nodes = 1
+num_nodes = 2
+num_vehicles = 1
+
+# Parameters
+C_ijk = 1
+C_R = 1
+C_F = 1
+C_T = 1
+Q_S = [1]
+Q_W = [1]
+L_S = [1]
+L_W = [0]
+T_ij = [[0, 1], [1, 0]]
+T_S_L = [8]
+T_S_U = [8.5]
+T_H_L = [7.75]
+T_H_U = [8.6]
+F = 1.15
+
+# Origin and destination
+o_k = [0]
+d_k = [3]
+
---
+++ model1.py
@@ -0,0 +1,100 @@
+import gurobipy as gp
+from gurobipy import GRB
+from gurobipy import GurobiError
+from gurobipy import quicksum
+from config import *
+
+try:
+    m = gp.Model("mip1")
+
+    pickups = range(num_pickup_nodes)
+    dropoffs = range(num_dropoff_nodes)
+    nodes = range(num_nodes)
+    vehicles = range(num_vehicles)
+
+    # Create variables
+    x = m.addVars(nodes, nodes, vehicles, vtype=GRB.BINARY, name="x")
+    w = m.addVars(pickups, vtype=GRB.BINARY, name="w")
+    q_S = m.addVars(nodes, vehicles, vtype=GRB.INTEGER, name="q_S")
+    q_W = m.addVars(nodes, vehicles, vtype=GRB.INTEGER, name="q_W")
+    t = m.addVars(nodes, vehicles, name="t")
+    l = m.addVars(nodes, name="l")
+    u = m.addVars(nodes, name="u")
+    d = m.addVars(pickups, name="d")
+
+    # OBJECTIVE FUNCTION
+    m.setObjective(quicksum(C_ijk*x[i,j,k] for i in nodes for j in nodes for k in vehicles) + quicksum(C_R*(1 - quicksum(x[i,j,k] for j in nodes for k in vehicles)) for i in pickups) + quicksum(C_T*(l[i] + u[i]) for i in nodes) + quicksum(C_F*d[i] for i in pickups), GRB.MINIMIZE)
+
+
+
+    # FLOW CONSTRAINTS
+    # mangler o(k) og d(k) i summene
+    m.addConstrs((quicksum(x[o_k[k],j,k] for j in pickups) == quicksum(x[i,d_k[k],k] for i in dropoffs) == 1 for k in vehicles), name="Flow1")
+
+    # må sette n til noe
+    m.addConstrs((quicksum(x[i,j,k] for j in nodes) - quicksum(x[n+i,j,k] for j in nodes) == 0 for i in pickups for k in vehicles), name="Flow2")
+
+    m.addConstrs((quicksum(x[j,i,k] for j in nodes) - quicksum(x[i,j,k] for j in nodes) == 0 for i in nodes for k in vehicles), name="Flow3")
+
+
+
+    # STANDARD SEATS CAPACITY CONSTRAINTS
+    m.addConstrs((q_S[o_k[k],k] == 0 for k in vehicles), name="SCapacity1")
+
+    # Usikker på hva man gjør med arcs (i,j) in A
+    m.addConstrs((q_S[i,k] + L_S[j] - q_S[j,k] <= Q_S[k]*(1 - x[i,j,k]) for j in pickups for i in nodes for k in vehicles), name="SCapacity2")
+
+    # Usikker på hva man gjør med arcs (i,j) in A + n må settes til noe
+    m.addConstrs((q_S[i, k] - L_S[j] - q_S[n+j, k] <= Q_S[k] * (1 - x[i, j, k]) for j in pickups for i in nodes for k in vehicles), name="SCapacity3")
+
+    m.addConstrs((quicksum(L_S[i]*x[i,j,k] for j in nodes) <= q_S[i,k] <= quicksum(Q_S[k]*x[i,j,k] for j in nodes) for i in pickups for k in vehicles), name="SCapacity4")
+
+    # n må settes til noe
+    m.addConstrs((quicksum((Q_S[k] - L_S[i])*x[n+i,j,k] for j in nodes) >= q_S[n+i,k] >= 0 for i in pickups for k in vehicles), name="SCapacity5")
+
+
+
+    # WHEELCHAIR SEATS CAPACITY CONSTRAINTS
+    m.addConstrs((q_W[o_k[k], k] == 0 for k in vehicles), name="WCapacity1")
+
+    # Usikker på hva man gjør med arcs (i,j) in A
+    m.addConstrs((q_W[i, k] + L_W[j] - q_W[j, k] <= Q_W[k] * (1 - x[i, j, k]) for j in pickups for i in nodes for k in vehicles), name="WCapacity2")
+
+    # Usikker på hva man gjør med arcs (i,j) in A + n må settes til noe
+    m.addConstrs((q_W[i, k] - L_W[j] - q_W[n + j, k] <= Q_W[k] * (1 - x[i, j, k]) for j in pickups for i in nodes for k in vehicles), name="WCapacity3")
+
+    m.addConstrs((quicksum(L_W[i] * x[i, j, k] for j in nodes) <= q_W[i, k] <= quicksum(Q_W[k] * x[i, j, k] for j in nodes) for i in pickups for k in vehicles), name="WCapacity4")
+
+    # n må settes til noe
+    m.addConstrs((quicksum((Q_W[k] - L_W[i]) * x[n + i, j, k] for j in nodes) >= q_W[n + i, k] >= 0 for i in pickups for k in vehicles), name="WCapacity5")
+
+
+
+    # TIME WINDOW CONSTRAINTS
+    m.addConstrs((T_S_L[i] - l[i] <= t[i,k] <= T_S_U[i] + u[i] for i in nodes for k in vehicles), name="TimeWindow1")
+
+    m.addConstrs((T_H_L[i] <= t[i,k] <= T_H_U[i] for i in nodes for k in vehicles), name="TimeWindow2")
+
+    # Mij må settes
+    m.addConstrs((t[i,k] + T_ij[i,j] - t[j,k] <= M[i,j]*(1 - x[i,j,k]) for i in nodes for j in nodes for k in vehicles), name="TimeWindow3")
+
+    # n må settes
+    m.addConstrs((t[i,k] + T_ij[i,n+i] - t[n+i,k] <= 0 for i in pickups for k in vehicles), name="TimeWindow4")
+
+
+
+    # RIDE TIME CONSTRAINTS
+    # M og n må settes
+    m.addConstrs((t[n+i,k] - t[i,k] - (1 + F)*T_ij[i,n+i] <= M*w[i] for i in pickups for k in vehicles), name="RideTime1")
+
+    # M og n må settes
+    m.addConstrs((d[i] >= t[n+i,k] - t[i,k] -M*(1 - w[i]) for i in pickups for k in vehicles), name="RideTime2")
+
+
+
+    # RUN MODEL
+    #m.optimize()
+
+except GurobiError as e:
+    print('Error reported')
+    print(e.message)

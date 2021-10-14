f149f63f1143d4dd0f9ac040795d933dd820a0d1
amalieoe <amalieomholt@gmail.com>
2021-10-14 13:34:08 +0200

    Changes in config

--- config.py
+++ config.py
@@ -1,16 +1,18 @@
 # Model 1
 import numpy as np
 from datetime import datetime, timedelta
+from scipy.spatial import distance
 
 # Sets
 num_pickup_nodes = 3
 n = num_pickup_nodes
-num_vehicles = 1
+num_vehicles = 2
 num_nodes = 2 * n
 num_nodes_and_depots = 2 * num_vehicles + 2 * n
 
 # Parameters
 C_D = [1]
+'''
 D_ij = [
         [0, 1, 4, 1, 3, 3],
         [1, 0, 3, 2, 2, 2],
@@ -19,13 +21,43 @@ D_ij = [
         [3, 1, 2, 2, 0, 3],
         [3, 2, 4, 2, 3, 0]
         ]  # in kms
+'''
+Position = [
+        (2, 3),
+        (2, 6),
+        (12, 4),
+        (6, 10),
+        (9.5, 7.5),
+        (6.5, 1),
+        (0,0),
+        (0,0),
+        (0,0),
+        (0,0)
+]
+
+
+D_ij = distance.cdist(Position,Position, 'euclidean')
+print(D_ij)
+
+speed = 40
+
+T_ij = np.empty(shape=(num_nodes_and_depots, num_nodes_and_depots), dtype=timedelta)
+
+for i in range(num_nodes_and_depots):
+    for j in range(num_nodes_and_depots):
+        T_ij[i][j] = timedelta(hours=(D_ij[i][j]/speed))
+
+print(T_ij)
+
 #C_R = 10
 C_F = 1
 C_T = 1
-Q_S = [10]
-Q_W = [1]
+Q_S = [10, 4]
+Q_W = [1, 2]
 L_S = [1, 2, 5] #Load for each request
 L_W = [0, 1, 0] #Wheelchair load for request
+
+'''
 T_ij = [
     [timedelta(hours=0, minutes=0), timedelta(hours=0, minutes=7), timedelta(hours=0, minutes=7), timedelta(hours=0, minutes=5), timedelta(hours=0, minutes=7), timedelta(hours=0, minutes=6)],
     [timedelta(hours=0, minutes=7), timedelta(hours=0, minutes=0), timedelta(hours=0, minutes=6), timedelta(hours=0, minutes=5), timedelta(hours=0, minutes=5), timedelta(hours=0, minutes=5)],
@@ -34,7 +66,7 @@ T_ij = [
     [timedelta(hours=0, minutes=7), timedelta(hours=0, minutes=5), timedelta(hours=0, minutes=5), timedelta(hours=0, minutes=6), timedelta(hours=0, minutes=0), timedelta(hours=0, minutes=6)],
     [timedelta(hours=0, minutes=6), timedelta(hours=0, minutes=5), timedelta(hours=0, minutes=7), timedelta(hours=0, minutes=5), timedelta(hours=0, minutes=6), timedelta(hours=0, minutes=0)]
 ]
-
+'''
 T_S_L = [
     datetime.strptime("2021-10-13 09:00:00", "%Y-%m-%d %H:%M:%S"),
     datetime.strptime("2021-10-13 09:00:00", "%Y-%m-%d %H:%M:%S"),

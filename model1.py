1691f8e30845496408e34cdbf23e7f1a14e3b084
marialbr <marialbr@stud.ntnu.no>
2021-10-20 11:08:22 +0200

    fixed order of constraints

--- model1.py
+++ model1.py
@@ -81,11 +81,24 @@ class Model:
             # FLOW CONSTRAINTS
             m.addConstrs(
                 (
+                    quicksum(x[i, j, k] for j in nodes_depots for k in vehicles) == 1
+                    for i in pickups
+                ),
+                name="Flow1",
+            )
+
+            m.addConstrs(
+                (x[i, i, k] == 0 for i in nodes_depots for k in vehicles),
+                name="Flow2",
+            )
+
+            m.addConstrs(
+                (
                     quicksum(x[nodes_depots[2 * n + k], j, k] for j in nodes_depots)
                     == 1
                     for k in vehicles
                 ),
-                name="Flow1",
+                name="Flow3.1",
             )
 
             m.addConstrs(
@@ -97,64 +110,9 @@ class Model:
                     == 1
                     for k in vehicles
                 ),
-                name="Flow2",
+                name="Flow3.2",
             )
 
-            m.addConstrs(
-                (
-                    quicksum(x[j, nodes_depots[2 * n + k], k] for j in nodes_depots)
-                    == 0
-                    for k in vehicles
-                ),
-                name="Flow3",
-            )
-
-            m.addConstrs(
-                (
-                    quicksum(
-                        x[nodes_depots[2 * n + k + num_vehicles], i, k]
-                        for i in nodes_depots
-                    )
-                    == 0
-                    for k in vehicles
-                ),
-                name="Flow4",
-            )
-
-            m.addConstrs(
-                (
-                    quicksum(x[i, j, k] for j in nodes_depots)
-                    - quicksum(x[n + i, j, k] for j in nodes_depots)
-                    == 0
-                    for i in pickups
-                    for k in vehicles
-                ),
-                name="Flow5",
-            )
-            m.addConstrs(
-                (
-                    quicksum(x[j, i, k] for j in nodes_depots)
-                    - quicksum(x[i, j, k] for j in nodes_depots)
-                    == 0
-                    for i in nodes
-                    for k in vehicles
-                ),
-                name="Flow6",
-            )
-
-            m.addConstrs(
-                (x[i, i, k] == 0 for i in nodes_depots for k in vehicles),
-                name="Flow7",
-            )
-
-            m.addConstrs(
-                (
-                    quicksum(x[i, j, k] for j in nodes_depots for k in vehicles) == 1
-                    for i in pickups
-                ),
-                name="Flow8",
-            )
-            print("Flow9")
             # vehicles cannot drive into an origin
             m.addConstrs(
                 (
@@ -166,10 +124,9 @@ class Model:
                     == 0
                     for v in vehicles
                 ),
-                name="Flow9",
+                name="Flow4.1",
             )
 
-            print("Flow10")
             # vehicles cannot drive from a destination
             m.addConstrs(
                 (
@@ -181,11 +138,10 @@ class Model:
                     == 0
                     for v in vehicles
                 ),
-                name="Flow10",
+                name="Flow4.2",
             )
 
             # vehicles cannot drive from origins that are not their own
-            print("Flow11")
             m.addConstrs(
                 (
                     quicksum(
@@ -197,11 +153,10 @@ class Model:
                     == 0
                     for v in vehicles
                 ),
-                name="Flow11",
+                name="Flow5.1",
             )
 
             # vehicles cannot drive into destinations that are not their own
-            print("Flow12")
             m.addConstrs(
                 (
                     quicksum(
@@ -213,7 +168,28 @@ class Model:
                     == 0
                     for v in vehicles
                 ),
-                name="Flow12",
+                name="Flow5.2",
+            )
+
+            m.addConstrs(
+                (
+                    quicksum(x[i, j, k] for j in nodes_depots)
+                    - quicksum(x[n + i, j, k] for j in nodes_depots)
+                    == 0
+                    for i in pickups
+                    for k in vehicles
+                ),
+                name="Flow6",
+            )
+            m.addConstrs(
+                (
+                    quicksum(x[j, i, k] for j in nodes_depots)
+                    - quicksum(x[i, j, k] for j in nodes_depots)
+                    == 0
+                    for i in nodes
+                    for k in vehicles
+                ),
+                name="Flow7",
             )
 
             # STANDARD SEATS CAPACITY CONSTRAINTS
@@ -246,7 +222,7 @@ class Model:
                     for i in pickups
                     for k in vehicles
                 ),
-                name="SCapacity4",
+                name="SCapacity4.1",
             )
             m.addConstrs(
                 (
@@ -254,13 +230,22 @@ class Model:
                     for i in pickups
                     for k in vehicles
                 ),
-                name="SCapacity5",
+                name="SCapacity4.2",
             )
             m.addConstrs(
                 (
                     quicksum((Q_S[k] - L_S[i]) * x[n + i, j, k] for j in nodes_depots)
                     >= q_S[n + i, k]
                     for i in pickups
+                    for k in vehicles
+                ),
+                name="SCapacity5",
+            )
+
+            m.addConstrs(
+                (
+                    q_S[i, k] <= Q_S[k] * (1 - x[i, 2 * n + k + num_vehicles, k])
+                    for i in nodes_depots
                     for k in vehicles
                 ),
                 name="SCapacity6",
@@ -295,7 +280,7 @@ class Model:
                     for i in pickups
                     for k in vehicles
                 ),
-                name="WCapacity4",
+                name="WCapacity4.1",
             )
             m.addConstrs(
                 (
@@ -303,13 +288,22 @@ class Model:
                     for i in pickups
                     for k in vehicles
                 ),
-                name="WCapacity5",
+                name="WCapacity4.2",
             )
             m.addConstrs(
                 (
                     quicksum((Q_W[k] - L_W[i]) * x[n + i, j, k] for j in nodes_depots)
                     >= q_W[n + i, k]
                     for i in pickups
+                    for k in vehicles
+                ),
+                name="WCapacity5",
+            )
+
+            m.addConstrs(
+                (
+                    q_W[i, k] <= Q_W[k] * (1 - x[i, 2 * n + k + num_vehicles, k])
+                    for i in nodes_depots
                     for k in vehicles
                 ),
                 name="WCapacity6",
@@ -322,7 +316,7 @@ class Model:
                     for i in nodes
                     for k in vehicles
                 ),
-                name="TimeWindow1",
+                name="TimeWindow1.1",
             )
 
             m.addConstrs(
@@ -331,17 +325,17 @@ class Model:
                     for i in nodes
                     for k in vehicles
                 ),
-                name="TimeWindow2",
+                name="TimeWindow1.2",
             )
 
             m.addConstrs(
                 (T_H_L[i].timestamp() <= t[i, k] for i in nodes for k in vehicles),
-                name="TimeWindow3",
+                name="TimeWindow2.1",
             )
 
             m.addConstrs(
                 (t[i, k] <= T_H_U[i].timestamp() for i in nodes for k in vehicles),
-                name="TimeWindow4",
+                name="TimeWindow2.2",
             )
 
             m.addConstrs(
@@ -352,7 +346,7 @@ class Model:
                     for j in nodes
                     for k in vehicles
                 ),
-                name="TimeWindow5",
+                name="TimeWindow3",
             )
 
             m.addConstrs(
@@ -361,7 +355,7 @@ class Model:
                     for i in pickups
                     for k in vehicles
                 ),
-                name="TimeWindow6",
+                name="TimeWindow4",
             )
 
             # RIDE TIME CONSTRAINTS

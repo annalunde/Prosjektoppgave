d5a83f40589f1ee4d8912e218706b314724f35ff
Anna <annasofie.lunde@gmail.com>
2021-11-02 13:41:54 +0100

    with sklearn

--- Models/initial_config.py
+++ Models/initial_config.py
@@ -6,8 +6,8 @@ import numpy as np
 from datetime import datetime, timedelta
 
 # Sets
-n = 20  # number of pickup nodes
-num_vehicles = 3
+n = 24  # number of pickup nodes
+num_vehicles = 4
 num_nodes = 2 * n
 num_nodes_and_depots = 2 * num_vehicles + 2 * n
 
